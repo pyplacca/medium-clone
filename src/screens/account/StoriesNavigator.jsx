e763b364486f72f480b9fa52a967462edec11a9d
pyPlacca <dhegreatprosperity10@gmail.com>
2020-12-11 21:50:33 +0000

    replace "Stories.jsx" with "StoriesNavigator.jsx"
    This was necessary because the Stories screen houses other related screens hence, it's a navigator.
    
    *This component needs to be transformed into a navigator.*

---
+++ src/screens/account/StoriesNavigator.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { ScrollView, Text } from 'react-native';
+
+
+function StoriesNavigator () {
+	return (
+		<ScrollView>
+			<Text>Stories</Text>
+		</ScrollView>
+	)
+};
+
+export default StoriesNavigator;
