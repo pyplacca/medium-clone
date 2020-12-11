ae6b6d13ace4fa2364e1936da53b6091adb19079
pyPlacca <dhegreatprosperity10@gmail.com>
2020-12-11 22:05:48 +0000

    create initial Stories screen

---
+++ src/screens/account/stories/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { ScrollView, StyleSheet } from 'react-native';
+
+
+function Stories (props) {
+	return (
+		<ScrollView>
+
+		</ScrollView>
+	)
+};
+
+const styles = StyleSheet.create({
+
+});
+
+export default Stories;
