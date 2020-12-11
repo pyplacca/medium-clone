aabaa68b91150764619b0da002a536f28c6b08fc
pyPlacca <dhegreatprosperity10@gmail.com>
2020-12-11 21:45:41 +0000

    convert screens folder to a module

---
+++ src/screens/index.js
@@ -0,0 +1,9 @@
+import Activity from './Activity';
+import AccountNavigator from './AccountNavigator';
+import HomeNavigator from './HomeNavigator';
+
+export {
+	Activity,
+	AccountNavigator,
+	HomeNavigator
+}
