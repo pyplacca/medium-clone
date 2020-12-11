955aac891e34f3136f315b1aac9e81103ebb0776
pyPlacca <dhegreatprosperity10@gmail.com>
2020-12-11 21:51:34 +0000

    dark mode settings modal
    probably never going to use this

---
+++ src/screens/account/settings/DarkMode.jsx
@@ -0,0 +1,46 @@
+import React, { useContext } from 'react';
+import { View, Text } from 'react-native';
+import { SettingsContext } from '../../../context/appSettings';
+import { theme } from '../../../config';
+
+
+function DarkMode () {
+	const {settings, settingsDispatch} = useContext(SettingsContext);
+	const { colors } = theme[settings.darkMode ? 'dark' : 'light']
+
+	const textStyle = {
+		color: colors.foreground.primary
+	}
+
+	return (
+		<View
+			style={{
+				flex: 1,
+				alignItems: 'center',
+				justifyContent: 'center',
+			}}
+		>
+			<Text
+				style={textStyle}
+			>
+				Dark Mode setting
+			</Text>
+			<View>
+				<Text style={textStyle}>On</Text>
+				<View
+					accessibilityRole='radio'
+					// accessibilityState='checked'
+					style={{
+						borderColor: colors.background.tetiary,
+						width: 20,
+						height: 20,
+						borderWidth: 2,
+						backgroundColor: 'white'
+					}}
+				/>
+			</View>
+		</View>
+	)
+};
+
+export default DarkMode;
