589c90263eca3b9b67f8ee702e7a3baedbfad23a
pyPlacca <dhegreatprosperity10@gmail.com>
2020-12-11 21:39:23 +0000

    rename "utils.js" to "config.js"

---
+++ src/config.js
@@ -0,0 +1,58 @@
+export const theme = {
+	dark: {
+		colors: {
+			background: {
+				primary: '#000',
+				secondary: '#292929',
+			},
+			foreground: {
+				tetiary: '#4a9b56',
+				primary: '#fafafa',
+				secondary: '#9d9d9d',
+			}
+		}
+	},
+
+	light: {
+		colors: {
+			background: {
+				secondary: '#f2f2f2',
+				primary: '#fff',
+			},
+			foreground: {
+				primary: '#292929',
+				secondary: '#6f6f6f',
+				tetiary: '#258122',
+			}
+		}
+	}
+};
+
+export const measure = {
+	xs: 5,
+	s: 15,
+	m: 40,
+	l: 70,
+	xl: 100,
+};
+
+export const screenModalOptions = {
+	cardStyle: {
+		backgroundColor: 'transparent',
+	},
+	cardStyleInterpolator: ({ current: { progress } }) => ({
+    cardStyle: {
+      opacity: progress.interpolate({
+        inputRange: [0, 0.5, 0.9, 1],
+        outputRange: [0, 0.25, 0.7, 1],
+      }),
+    },
+    overlayStyle: {
+      opacity: progress.interpolate({
+        inputRange: [0, 1],
+        outputRange: [0, 0.5],
+        extrapolate: 'clamp',
+      }),
+    },
+  }),
+}
