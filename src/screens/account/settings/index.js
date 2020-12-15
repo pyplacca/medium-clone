1b2287e6120c373b283f09580d17a00c1c01ee59
pyPlacca <dhegreatprosperity10@gmail.com>
2020-12-15 22:49:38 +0000

    update Settings landing screen

--- src/screens/account/settings/index.js
+++ src/screens/account/settings/index.js
@@ -1,43 +1,20 @@
 import React, { useLayoutEffect, useContext } from 'react';
 import { ScrollView, View, Text, Pressable, StyleSheet } from 'react-native';
-// import { useFocusEffect } from '@react-navigation/native';
+import { MaterialIcons } from '@expo/vector-icons';
 import { getFocusedRouteNameFromRoute } from '@react-navigation/native';
 import { SimpleHeader } from '../../../components/headers';
-import { SettingsContext } from '../../../context/appSettings';
+import { ButtonRow, Gap, CheckBox } from '../../../components';
+import { AppContext } from '../../../context';
 import { theme, measure } from '../../../config';
+// screens
+import DarkMode from './DarkMode';
+import Beta from './Beta';
 
-
-const Gap = () => <View style={styles.gap}/>
 
 function Settings ({navigation, route}) {
 
-	const { settings: {darkMode}, settingsDispatch } = useContext(SettingsContext);
-	const { foreground: fg, background: bg } = theme[darkMode ? 'dark' : 'light'].colors
-
-	function RowItem ({text, style, textStyle, children, ...props}) {
-		return (
-			<Pressable
-				{...props}
-				android_ripple={{
-					color: theme.dark.colors.foreground.secondary
-				}}
-				style={[
-					styles.row,
-					{ backgroundColor: bg.primary },
-					style
-				]}
-			>
-				{
-					text ?
-					<Text style={[{color: fg.primary}, textStyle]}>
-						{text}
-					</Text>
-					: null
-				}
-				{ children }
-			</Pressable>
-		)
-	}
+	const { state, dispatch } = useContext(AppContext);
+	const { foreground: fg, background: bg } = theme[state.darkMode ? 'dark' : 'light'].colors
 
 	const sds = {
 		emphasis: { color: fg.tetiary },
@@ -46,15 +23,27 @@ function Settings ({navigation, route}) {
 			color: fg.secondary,
 			paddingHorizontal: measure.s + 2,
 			marginBottom: measure.s,
+		},
+		checkbox: {
+			borderColor: fg.secondary,
+			backgroundColor: fg.tetiary,
 		}
 	}
 
-	const toggleDarkMode = () => {
-		settingsDispatch({
-			type: 'DARK_MODE',
-			payload: !darkMode
-		})
-		navigation.navigate('dark_mode')
+	// const toggleDarkMode = () => {
+	// 	dispatch({
+	// 		type: 'DARK_MODE',
+	// 		payload: !darkMode
+	// 	})
+	// 	// navigation.navigate('dark_mode')
+	// }
+
+	const sendDispatch = (type, payload) => {
+		dispatch({type, payload})
+	}
+
+	const goTo = screen => {
+		navigation.navigate(screen)
 	}
 
 	return (
@@ -62,7 +51,7 @@ function Settings ({navigation, route}) {
 			<ScrollView>
 				<SimpleHeader title='Settings'/>
 				<Gap/>
-				<RowItem
+				<ButtonRow
 					text='Become a member'
 					textStyle={sds.emphasis}
 				/>
@@ -70,75 +59,89 @@ function Settings ({navigation, route}) {
 
 				{/* Configure Medium */}
 				<Text style={sds.lead}>Configure Medium</Text>
-				<RowItem text='Customize your interests' />
-				<RowItem onPress={toggleDarkMode}>
+				<ButtonRow text='Customize your interests' />
+				<ButtonRow onPress={() => sendDispatch('DARK_MODE', !state.darkMode)}>
 					<Text style={sds.text}>Dark Mode</Text>
-					<Text style={sds.emphasis}>{darkMode ? 'On' : 'Off'}</Text>
-				</RowItem>
-				<RowItem text='Push Notifications' />
-				<RowItem text='Email Notifications' />
+					<Text style={sds.emphasis}>{state.darkMode ? 'On' : 'Off'}</Text>
+				</ButtonRow>
+				<ButtonRow text='Push Notifications' />
+				<ButtonRow text='Email Notifications' />
 
 				<Gap/>
-				<RowItem text='Stats'/>
+				<ButtonRow text='Stats'/>
 				<Gap/>
 
-				<RowItem text='Account' />
-				<RowItem>
+				<ButtonRow text='Account' />
+				<ButtonRow onPress={() => goTo('beta')}>
 					<Text style={sds.text}>Toggle Medium Beta</Text>
-				</RowItem>
+					<MaterialIcons name="chevron-right" size={20} color={fg.secondary} />
+				</ButtonRow>
 				<Gap/>
 
 				{/* Social */}
 				<Text style={sds.lead}>Social</Text>
-				<RowItem text='Join our community' />
-				<RowItem>
+				<ButtonRow text='Join our community' />
+				<ButtonRow>
 					<View>
 						<Text style={sds.text}>Twitter</Text>
 					</View>
 					<Text style={sds.emphasis}>Connect</Text>
-				</RowItem>
-				<RowItem>
+				</ButtonRow>
+				<ButtonRow>
 					<View>
 						<Text style={sds.text}>Facebook</Text>
 					</View>
 					<Text style={sds.emphasis}>Connect</Text>
-				</RowItem>
+				</ButtonRow>
 				<Gap/>
 
 				{/* About Medium */}
 				<Text style={sds.lead}>About Medium</Text>
-				<RowItem text='Help'/>
-				<RowItem text='Terms of service'/>
-				<RowItem text='Privacy Policy'/>
+				<ButtonRow text='Help'/>
+				<ButtonRow text='Terms of service'/>
+				<ButtonRow text='Privacy Policy'/>
 				<Gap/>
 
 				{/* App Control */}
 				<Text style={sds.lead}>App Control</Text>
-				<RowItem>
+				<ButtonRow
+					onPress={() => sendDispatch(
+						'IMAGE_LOADING',
+						!state.imageLoadingDisabled
+					)}
+				>
 					<Text style={sds.text}>Disabled image loading</Text>
-				</RowItem>
-				<RowItem>
+					<CheckBox
+						style={sds.checkbox}
+						checkColor={bg.primary}
+						checked={state.imageLoadingDisabled}
+					/>
+				</ButtonRow>
+				<ButtonRow
+					onPress={() => sendDispatch(
+						'BACKGROUND_DATA_USAGE',
+						!state.wifiOnlyBackgroundDataUsage
+					)}
+				>
 					<Text style={sds.text}>Limit background data usage to wifi only</Text>
-				</RowItem>
+					<CheckBox
+						style={sds.checkbox}
+						checkColor={bg.primary}
+						checked={state.wifiOnlyBackgroundDataUsage}
+					/>
+				</ButtonRow>
 				<Gap/>
-				<RowItem text='Sign out' style={{marginBottom: 0}}/>
+				<ButtonRow text='Sign out' style={{marginBottom: 0}}/>
 			</ScrollView>
 		</View>
 	)
 };
 
 const styles = StyleSheet.create({
-	row: {
-		flexDirection: 'row',
-		justifyContent: 'space-between',
-		alignItems: 'center',
-		padding: measure.s + 2,
-		marginBottom: 1
-	},
-
-	gap: {
-		height: measure.s + measure.xs + 2
-	},
 });
 
 export default Settings;
+export {
+	DarkMode,
+	Beta
+};
