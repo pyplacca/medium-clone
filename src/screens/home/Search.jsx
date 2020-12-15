4be62c13e24498a7e8619958cc1b517c4ba8e5a8
pyPlacca <dhegreatprosperity10@gmail.com>
2020-12-15 23:24:42 +0000

    Update Search.jsx

---
+++ src/screens/home/Search.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { ScrollView, Text, StyleSheet } from 'react-native'
+
+
+export default function Search () {
+	return (
+		<ScrollView>
+			<Text>Search</Text>
+		</ScrollView>
+	)
+}
