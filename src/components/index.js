30f03622e67982dc50c08fae415d8ea46f608a79
pyPlacca <dhegreatprosperity10@gmail.com>
2020-12-15 22:45:23 +0000

    Update components module

--- src/components/index.js
+++ src/components/index.js
@@ -1,4 +1,8 @@
 export { default as FailedToConnect } from './FailedToConnect';
-export { default as StackNavigator } from './StackNavigator';
 export { default as Navigation } from './Navigation';
+export { default as ButtonRow } from './ButtonRow';
+export { default as UserIcon } from './UserIcon';
+export { default as Creator } from './Creator';
+export { default as Icon } from './Icon';
+export * from './Misc';
 // export * from './home';
