989dfc9795a5abae4ab7eb6bee18264f6dd15d4f
pyPlacca <dhegreatprosperity10@gmail.com>
2020-12-15 23:21:47 +0000

    Update App.js

--- App.js
+++ App.js
@@ -1,18 +1,36 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useFonts } from '@use-expo/font';
 import { StatusBar } from 'react-native';
-import SettingsProvider from './src/context/appSettings';
+import { Asset } from 'expo-asset';
+import AppLoading from 'expo-app-loading';
+import AppProvider from './src/context';
 import AppNavigator from './src/AppNavigator';
 
 export default function App () {
-  const [loaded] = useFonts({
+	// load fonts
+  const [loaded] = useFonts({});
+  // const [isReady, setIsReady] = useState(false);
 
-  });
+  // const _cacheResourcesAsync = async () => {
+  //   const images = [require('./assets/logo-white.png')];
 
-  return !loaded ? null : (
-    <SettingsProvider>
+  //   const cacheImages = images.map(image => {
+  //     return Asset.fromModule(image).downloadAsync();
+  //   });
+  //   return Promise.all(cacheImages);
+  // }
+
+  return !loaded /*&& !isReady*/ ? (
+  	// show loading screen
+	  <AppLoading
+	    // startAsync={_cacheResourcesAsync}
+	    // onFinish={() => setIsReady(true)}
+	    // onError={console.warn}
+	  />
+  ) : (
+    <AppProvider>
       <StatusBar style='dark'/>
       <AppNavigator />
-    </SettingsProvider>
+    </AppProvider>
   );
 };
