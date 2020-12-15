3d74447decd998a14aabe4e123d5965fd5125e1d
pyPlacca <dhegreatprosperity10@gmail.com>
2020-12-15 22:45:52 +0000

    Create Beta.jsx

---
+++ src/screens/account/settings/Beta.jsx
@@ -0,0 +1,54 @@
+import React, { useContext } from 'react';
+import { View, Text } from 'react-native';
+import { Gap, ButtonRow, RadioButton } from '../../../components';
+import { StackHeader } from '../../../components/headers';
+import { AppContext } from '../../../context';
+import { theme } from '../../../config';
+
+
+function Beta ({navigation}) {
+
+	const { state, dispatch: dispatch } = useContext(AppContext);
+	const { colors } = theme[state.darkMode ? 'dark' : 'light'];
+
+	const toggleBeta = type => {
+		dispatch({
+			type: 'BETA',
+			payload: type
+		})
+	}
+
+	return (
+		<View
+			style={{
+				backgroundColor: colors.background.secondary,
+				flex: 1
+			}}
+		>
+			<StackHeader title='Beta' centerTitle navigation={navigation}/>
+			<Gap/>
+			<ButtonRow onPress={() => toggleBeta('beta')}>
+				<Text style={{color: colors.foreground.primary}}>
+					Medium Beta
+				</Text>
+				<RadioButton
+					color={colors.foreground.secondary}
+					checked={state.beta === 'beta'}
+				/>
+			</ButtonRow>
+			<ButtonRow onPress={() => toggleBeta('classic')}>
+				<Text style={{color: colors.foreground.primary}}>
+					Medium Classic
+				</Text>
+				<RadioButton
+					color={colors.foreground.secondary}
+					checked={state.beta === 'classic'}
+				/>
+			</ButtonRow>
+			<Gap/>
+			<ButtonRow text='Send us feedback on the beta'/>
+		</View>
+	)
+};
+
+export default Beta;
