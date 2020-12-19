72ee87099387b23ee1e2c9d341dee65f3505dc6d
pyPlacca <dhegreatprosperity10@gmail.com>
2020-12-19 12:18:52 +0000

    add configuration for absolute imports

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,20 @@
 module.exports = function(api) {
   api.cache(true);
   return {
-    presets: ['babel-preset-expo'],
+		presets: ['babel-preset-expo'],
+		plugins: [
+			[
+				'module-resolver', {
+					root: ['./src', './assets'],
+					extensions: ['.jsx', '.js', '.ios.js', '.android.js'],
+				}
+			],
+			[
+				'babel-plugin-root-import', {
+					rootPathPrefix: '#',
+					rootPathSuffix: 'src'
+				}
+			]
+		]
   };
 };
